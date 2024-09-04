7f148e6c0865caa6b2995edc5640aa4b124b162a
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-09-04 16:30:05 +0200

    Initialisation de l'API avec dépendance et 1rst Entity"doctrine/dbal": "^3",         "doctrine/doctrine-bundle": "^2.13",         "doctrine/doctrine-migrations-bundle": "^3.3",         "doctrine/orm": "^3.2",

---
+++ src/Entity/Advert.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdvertRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdvertRepository::class)]
+class Advert
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adress = null;
+
+    #[ORM\Column]
+    private ?int $nbRoom = null;
+
+    #[ORM\Column]
+    private ?float $surfaceArea = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $rating = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): static
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getNbRoom(): ?int
+    {
+        return $this->nbRoom;
+    }
+
+    public function setNbRoom(int $nbRoom): static
+    {
+        $this->nbRoom = $nbRoom;
+
+        return $this;
+    }
+
+    public function getSurfaceArea(): ?float
+    {
+        return $this->surfaceArea;
+    }
+
+    public function setSurfaceArea(float $surfaceArea): static
+    {
+        $this->surfaceArea = $surfaceArea;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getRating(): ?float
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?float $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+}
