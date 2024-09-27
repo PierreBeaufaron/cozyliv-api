acdfbc19a2f126db1774418762c83c130b803a5d
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-09-27 16:54:33 +0200

    Fixture complètes !

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Advert;
+use App\Entity\AdvertImg;
 use App\Entity\City;
 use App\Entity\Country;
+use App\Entity\Room;
+use App\Entity\Service;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -17,7 +21,11 @@ class AppFixtures extends Fixture
     private array $dbCountries = [];
     private array $dbCities = [];
     private array $dbUsers = [];
-
+    private array $dbOwners = [];
+    private array $dbServices = [];
+    private array $dbAdvertServices = [];
+    private array $dbRoomServices = [];
+    private array $dbAdverts = [];
     public function __construct(
         private SerializerInterface $serializer,
         private UserPasswordHasherInterface $hasher
@@ -33,7 +41,10 @@ class AppFixtures extends Fixture
         $this->loadCountries($manager);
         $this->loadCities($manager);
         $this->loadUsers($manager, $faker);
-
+        $this->loadServices($manager);
+        $this->loadAdverts($manager, $faker);
+        $this->loadRooms($manager, $faker);
+        $this->loadAdvertImg($manager);
         $manager->flush();
     }
 
@@ -55,9 +66,9 @@ class AppFixtures extends Fixture
     private function loadCities(ObjectManager $manager): void
     {
         // Cities
-        $rawCities = file_get_contents(__DIR__ . '/cities.json');
+        $rawCitiesContent = file_get_contents(__DIR__ . '/cities.json');
         /** @var City[] $cities */
-        $cities = $this->serializer->deserialize($rawCities, City::class . '[]', 'json');
+        $cities = $this->serializer->deserialize($rawCitiesContent, City::class . '[]', 'json');
 
         foreach ($cities as $city) {
             $city->setCountry($this->dbCountries[7]);
@@ -83,7 +94,100 @@ class AppFixtures extends Fixture
 
                 $manager->persist($user);
                 $this->dbUsers[] = $user;
+                $userRoles = $user->getRoles();
+                if ($userRoles[0] == "ROLE_OWNER") {
+                    $this->dbOwners[] = $user;
+                }             
         }    
+        // dd($this->dbOwners);
     }
+
+    public function loadServices(ObjectManager $manager): void
+    {
+        // Services
+        $rawServiceContent = file_get_contents(__DIR__ . '/services.json');
+        /** @var Service[] $services */
+        $services = $this->serializer->deserialize($rawServiceContent, Service::class . '[]', 'json');
+
+        foreach ($services as $service) {
+                $manager->persist($service);
+                $this->dbServices[] = $service;
+                if ($service->getType() !== 3){
+                    $this->dbAdvertServices[] = $service;
+                } else {
+                    $this->dbRoomServices[] = $service;
+                }
+        }    
+        // dump($this->dbAdvertServices);
+        // dd($this->dbRoomServices);
+    }
+
+    public function loadAdverts(ObjectManager $manager, Generator $faker): void
+    {
+        // Adverts
+        $rawAdvertContent = file_get_contents(__DIR__ . '/adverts.json');
+        /** @var Advert[] $adverts */
+        $adverts = $this->serializer->deserialize($rawAdvertContent, Advert::class . '[]', 'json');
+
+        foreach ($adverts as $advert) {
+            $advert
+                ->setNbRoom($faker->numberBetween(2, 8))
+                ->setSurfaceArea($faker->numberBetween(65, 250))
+                ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-3 years')))
+                ->setCity($faker->randomElement($this->dbCities))
+                ->setOwner($faker->randomElement($this->dbOwners));
+
+                // Récupérer un nombre aléatoire de services à partir de dbAdvertServices sans doublon
+                $randomServices = $faker->randomElements($this->dbAdvertServices, $faker->numberBetween(8, 25));
+                // Ajouter les services générés
+                foreach ($randomServices as $service) {
+                    $advert->addService($service);
+                }   
+
+                $manager->persist($advert);
+                $this->dbAdverts[] = $advert;
+        }
+    }
+
+    public function loadRooms(ObjectManager $manager, Generator $faker): void
+    {
+        // Rooms
+        $rawRoomContent = file_get_contents(__DIR__ . '/rooms.json');
+        /** @var Room[] $rooms */
+        $rooms = $this->serializer->deserialize($rawRoomContent, Room::class . '[]', 'json');
+
+        foreach ($rooms as $room) {
+            $room
+                ->setAdvert($faker->randomElement($this->dbAdverts));
+
+                $randomServices = $faker->randomElements($this->dbRoomServices, $faker->numberBetween(2, 5));
+                foreach ($randomServices as $service) {
+                    $room->addService($service);
+                }
+
+                $manager->persist($room);
+        }
+    }
+
+    public function loadAdvertImg(ObjectManager $manager): void
+    {
+        // AdvertImg
+        $rawAdvertImgContent = file_get_contents(__DIR__ . '/advertImg.json');
+        /** @var AdvertImg[] $advertImgs */
+        $advertImgs = $this->serializer->deserialize($rawAdvertImgContent, AdvertImg::class . '[]', 'json');
+
+        $imgIndex = 0;
+        for($i = 0; $i < (count($this->dbAdverts) - 1); $i++) {
+            for ($j = 0; $j<5; $j++) {
+                $advertImg = $advertImgs[$imgIndex];
+                $advertImg
+                    ->setAdvert($this->dbAdverts[$i]);
+    
+                $manager->persist($advertImg);
+                $imgIndex++;
+            }
+        }
+
+    } 
 
 }
