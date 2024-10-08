3bde0b5b9db62f58dab0e93d19fe9d6fd7588378
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-10-08 09:45:32 +0200

    Listener JWT ajout de data

---
+++ src/EventListener/JwtCreatedListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+final class JwtCreatedListener
+{
+    #[AsEventListener(event: 'JWTCreated')]
+    // Écoute l'événement JWTCreated pour ajouter des propriétés au token.
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        // Récupère les données actuelles du payload de token
+        $data = $event->getData();
+
+        // Récupère l'utilisateur actuel
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        // Ajouter le prénom et nom
+        $data['firstname'] = $user->getFirstname();
+        $data['lastname'] = $user->getLastname();
+
+        // Mets à jour le payload avec les nouvelles données
+        $event->setData($data);
+    }
+}
